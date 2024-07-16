7072808606a4e898b967071318bb6d57957ed103
ffloof <prteza@gmail.com>
2024-07-16 16:52:43 -0700

    Bring in offline changes going to move to other repo next

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # CMIP Download Script
 
+"Simple" script to automate mass downloads of CMIP data.
+
+To use start by going to the [ESGF website](https://aims2.llnl.gov/search/?project=CMIP6/) and adding all filters as usual until only the datasets of interest are left.
+
+Then click the "Copy Search" button to get the search url.
+
+![image](docs/button.png)
+
+To run the script and download the files
+
+`python3 -o [output folder] main.py -u '[paste the url here]'`
+
+So for example...
+
+`python3 -o ./MyFavoriteCmipDataFolder -u 'https://aims2.llnl.gov/search/?project=CMIP6&activeFacets=%7B%22experiment_id%22%3A%221pctCO2-rad%22%2C%22variable_id%22%3A%22gpp%22%7D'`
+
+Make sure to include the '' around the url. This will download every file shown on the website so make sure you filter down to only what you want first!
+
 
--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 import urllib.request
+from urllib.parse import quote
 import json
 from pathlib import Path
+import time
 
 # Support for command line arugments
 import argparse
 parser = argparse.ArgumentParser(description="Script to download data from CMIP")
-parser.add_argument("-u", "--url", type=str, help="Query URL for data to be downloaded, more info in README.md", default="http://esgf-node.llnl.gov/esg-search/search/?offset=0&limit=10&type=Dataset&replica=false&latest=true&source_id=CESM2&experiment_id=ssp370&frequency=mon&variable_id=gpp%2Churs%2Cps%2Ctran&project=CMIP6&facets=mip_era%2Cactivity_id%2Cproduct%2Csource_id%2Cinstitution_id%2Csource_type%2Cnominal_resolution%2Cexperiment_id%2Csub_experiment_id%2Cvariant_label%2Cgrid_label%2Ctable_id%2Cfrequency%2Crealm%2Cvariable_id%2Ccf_standard_name%2Cdata_node&format=application%2Fsolr%2Bjson")
-parser.add_argument("-o", "--output", type=str, help="Path of folder/directory that files will be downloaded to", default="./cmipdata/")
+parser.add_argument("-u", "--url", type=str, help="Query URL for data to be downloaded, more info in README.md", default="https://aims2.llnl.gov/search/?project=CMIP6&activeFacets=%7B%22experiment_id%22%3A%221pctCO2-rad%22%2C%22variable_id%22%3A%22gpp%22%7D")
+parser.add_argument("-o", "--output", type=str, help="Path of folder/directory that files will be downloaded to", default="./cmipdata/") 
 args = parser.parse_args()
 
 # Any dataset from this search url will be pulled
 # For more info check README.md
 searchurl = args.url
+print(searchurl)
+#print(searchurl.split("activeFacets"))
 
+# Have to do a bit of manipulation to reformat it into the format that gets us .json metadata
+
+params = json.loads(searchurl.split("activeFacets=")[1].replace("%22", "\"").replace("%7B", "{").replace("%3A", ":").replace("%2C", ",").replace("%7D", "}"))
+
+templateurl = "https://aims2.llnl.gov/metagrid-backend/proxy/search?project=CMIP6&offset=0&limit=10000&type=Dataset&format=application%2Fsolr%2Bjson&facets=activity_id%2C+data_node%2C+source_id%2C+institution_id%2C+source_type%2C+experiment_id%2C+sub_experiment_id%2C+nominal_resolution%2C+variant_label%2C+grid_label%2C+table_id%2C+frequency%2C+realm%2C+variable_id%2C+cf_standard_name&latest=true&query=*"  #&experiment_id=1pctCO2-rad&variable_id=cVeg
+
+
+
+templateaddon = ""
+for (key, value) in params.items():
+	templateaddon += "&" + key + "=" + value
+
+searchurl = templateurl + quote(templateaddon, safe="&=")
+#print(searchurl)
+
+
+# First searches esgf metagrid for datasets matching given the paramaters
+# Then for each dataset it finds the download urls
 download_urls = []
 with urllib.request.urlopen(searchurl) as query:
-	# Fetch info about datasets matching searchurl
 	query_info = json.load(query)["response"]
-	print("Query found:", query_info["numFound"], "datasets")
+
+
+	print("Query found:", query_info["numFound"], "nodes")
+	print("Indexing files...")
 
 	# For each dataset in query...
 	for dataset_info in query_info["docs"]:
 		# Get the id of each dataset and make another request to get the download urls
 		id = dataset_info["id"]
-		with urllib.request.urlopen("https://esgf-node.llnl.gov/search_files/"+id+"/esgf-node.llnl.gov/") as subquery:
+		with urllib.request.urlopen("https://aims2.llnl.gov/metagrid-backend/proxy/search?dataset_id="+id+"&format=application%2Fsolr%2Bjson&limit=10&offset=0&type=File&") as subquery:
 			dataset_metadata = json.load(subquery)
 			# For each file in the dataset, we add the download url to our list of downloard_urls
 			for file_metadata in dataset_metadata["response"]["docs"]:
@@ -31,21 +55,52 @@ with urllib.request.urlopen(searchurl) as query:
 				for download_url in file_metadata["url"]:
 					if download_url.startswith("http"):
 						download_urls.append(download_url.split('|')[0])
+						# TODO: add flag to filter files meta with .html
+
 
 # Folder to download to, if it doesn't exist create it
 path = args.output
 Path(path).mkdir(parents=True, exist_ok=True)
 
-# Download each of the files...
-i = 1
-print("Query found:", len(download_urls), "files")
+
+# There are duplicate files since there are often duplicates hosted by different regions/servers
+# We map filename to possible urls, so we only download it once
+# And if the download doesn't work we can move on to the next candidate url
+
+urlmap = {}
+
 for url in download_urls:
-	#print(url)
 	file_name = url.rsplit('/', 1)[-1]
-	print("GET", "(",i,"/",len(download_urls),")", file_name)
-	try:
-		urllib.request.urlretrieve(url, path + file_name)
-		print("COMPLETE")
-	except Exception as err:
-		print("FAILED", err)
-	i += 1
+	if (urlmap.get(file_name)) == None:
+		urlmap[file_name] = []
+	urlmap[file_name].append(url)
+
+
+# Download each of the files...
+#print(urlmap)
+print("Query found:", len(urlmap), "files")
+
+progress_counter = 1
+for key in urlmap.keys():
+	urllist = urlmap[key]
+	for url in urllist:
+		time.sleep(1) # Don't get rate limited'
+		print("GET", "(",progress_counter,"/",len(urlmap),")", key)
+		# print(url)
+		try:
+			urllib.request.urlretrieve(url, path + key)
+
+			print("COMPLETE")
+			progress_counter += 1
+			break
+		except Exception as err:
+			print("FAILED", err)
+
+
+
+
+
+
+
+
+
